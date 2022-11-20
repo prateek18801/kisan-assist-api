873268b0e92e92db95e102f7b3dfe6bea58d417c
Prateek Chaurasia <prateek18801@gmail.com>
2022-11-20 15:59:31 +0530

    add register produce controller

--- api/controllers/controller.js
+++ api/controllers/controller.js
@@ -1,4 +1,27 @@
-exports.getAnswer = (req, res, next) => {
+const Produce = require('../models/produce');
+
+const { extractProduceName, createProducePrompt } = require('../utils/openai');
+
+exports.getAnswer = async (req, res, next) => {
     const query = req.query.q;
-    
+
+}
+
+
+exports.registerProduce = async (req, res, next) => {
+    const { produce_name, description } = req.body;
+    console.log(req.body);
+    try {
+        const saved = await new Produce({
+            produce_name: produce_name.toLowerCase(),
+            description
+        }).save();
+
+        return res.status(201).json({
+            ok: true,
+            message: 'created'
+        });
+    } catch (err) {
+        next(err);
+    }
 }
