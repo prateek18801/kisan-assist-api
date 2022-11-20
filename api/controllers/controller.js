9d2d8549aafbf62b0736db1640bf19116dc12852
Prateek Chaurasia <prateek18801@gmail.com>
2022-11-20 19:34:32 +0530

    update answer controller

--- api/controllers/controller.js
+++ api/controllers/controller.js
@@ -1,25 +1,47 @@
 const Produce = require('../models/produce');
 
-const { extractProduceName, createProducePrompt } = require('../utils/openai');
+const { extractProduceName, createProducePrompt, resolveQuery } = require('../utils/openai');
 
 exports.getAnswer = async (req, res, next) => {
-    const query = req.query.q;
+    const query = req.query.q.replaceAll(/\?/g, '');
+    try {
+        // extract the produce name from the incoming query
+        const produce = await extractProduceName(query);
+        
+        // find the prompt for the produce in db
+        let saved = await Produce.findOne({ produce_name: produce });
 
+        // if prompt does not exist create a prompt
+        if (!saved) {
+            saved = await createProducePrompt(produce);
+        }
+
+        // generate response from prompt and query
+        const response = await resolveQuery(saved, query);
+
+        return res.status(200).json({
+            ok: true,
+            message: response
+        });
+
+    } catch (err) {
+        next(err);
+    }
 }
 
 
 exports.registerProduce = async (req, res, next) => {
     const { produce_name, description } = req.body;
-    console.log(req.body);
     try {
         const saved = await new Produce({
             produce_name: produce_name.toLowerCase(),
-            description
+            description: description.replaceAll(/ Q\. /g, '\nQ. ').replaceAll(/ A\. /g, '\nA. ')
         }).save();
 
         return res.status(201).json({
             ok: true,
-            message: 'created'
+            message: 'created',
+            data: saved
         });
     } catch (err) {
         next(err);
