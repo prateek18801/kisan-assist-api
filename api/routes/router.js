a04468590513307be4934a2810ce05e57e423fae
Prateek Chaurasia <prateek18801@gmail.com>
2022-11-20 16:00:24 +0530

    add register produce route

--- api/routes/router.js
+++ api/routes/router.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 
 const controller = require('../controllers/controller');
 
-router.get('/answer', controller.getAnswer);
+router.get('/v1/answer', controller.getAnswer);
+
+router.post('/v1/produce', controller.registerProduce);
 
 module.exports = router;
