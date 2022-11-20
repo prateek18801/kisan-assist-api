7988628e1d2a9dffcf0696fcfdcfb009a3d58359
Prateek Chaurasia <prateek18801@gmail.com>
2022-11-21 02:28:02 +0530

    add cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./api/routes/router');
 require('./api/utils/db').connect();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
