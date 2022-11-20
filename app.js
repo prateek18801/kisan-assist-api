97edc6687b9b619ca6fd3f624987678eb13f64a5
Prateek Chaurasia <prateek18801@gmail.com>
2022-11-20 06:01:05 +0530

    add router

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const router = require('./api/routes/router');
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use('/api', router);
 
 app.listen(process.env.PORT, ()=> {
     console.log(`server running on PORT:${process.env.PORT}`);
